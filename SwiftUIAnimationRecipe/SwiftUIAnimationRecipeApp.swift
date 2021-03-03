7a6e45f0db2d0a2f51a6230e4941521117347b13
Shohei-Yamamoto <shoheiyamamotoroom@gmail.com>
2021-03-03 21:37:34 +0900

    initial commit

---
+++ SwiftUIAnimationRecipe/SwiftUIAnimationRecipeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIAnimationRecipeApp.swift
+//  SwiftUIAnimationRecipe
+//
+//  Created by Shohei Yamamoto on 2021/03/03.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIAnimationRecipeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
